4985abfdc6aa30aea2669aa9d795fcc4855bd5e1
y4ser <Eng.yaseralshammari@gmail.com>
2025-10-11 21:15:38 +0300

    Add SmokeRunner class

---
+++ src/test/java/runners/SmokeRunner.java
@@ -0,0 +1,25 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+        tags = "@smoke",
+        dryRun = false,
+        plugin = {
+                "html:reports/html-reports/cucumber.html",
+                "json:reports/json-reports/cucumber.json",
+                "junit:reports/xml-reports/cucumber.xml",
+                "pretty",
+                "rerun:target/failedScenarios.txt",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" // For Spark report
+        }
+)
+
+public class SmokeRunner {
+}
